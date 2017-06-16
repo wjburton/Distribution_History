8b1b4296f51d0e3e14e6038a7ef8b51776f64385
Will Burton <wjburton@ncsu.edu>
2017-06-16 06:49:17 -0700

    set up R shiny framework, complete students t distribution research

--- Dist_Application/app.R
+++ Dist_Application/app.R
@@ -13,7 +13,25 @@ library(shiny)
 ui <- fluidPage(
   titlePanel(h1('Distribution History')),
   sidebarLayout( position = 'right',
-    sidebarPanel("Select a Distribution"),
+    sidebarPanel(
+      selectInput('dist_dropdown', 'Select a Distribution:', 
+                  choices = list('students t-distribution', 'normal distribution',
+                                 'standard normal distribution', 'F distribution',
+                                 'chi-squared distribution', 'weibull distribution',
+                                 'logistic distribution', 'gamma distribution',
+                                 'poisson distribution', 'uniform distribution',
+                                 'triangular distribution', 'binomial distribution', 
+                                 'exponential distribution', 'bernoulli distribution',
+                                 'geometric distribution', 'generalized gamma distribution',
+                                 'doubly Non-Central F-Distribution', 'doubly Non-Central t-Distribution',
+                                 'compound poisson distribution', 'binormal distribution',
+                                 'extreme value distribution', 'cauchy distribution',
+                                 'binormal distribution', 'beta distribution',
+                                 'triangular distribution', 'logarithmic distribtion',
+                                 'multinomial distribution', 'negative binomial distribution',
+                                 'hypergeometric distribution', 'hyperexponential distribution',
+                                 'Pareto Distribution'))
+    ),
     mainPanel(h4('The purpose of this application is to be a learning tool to understand
                   some of the commonly used statistical distributions. Specifically it covers:'),br(),
               p('1. The History of the distribution'),
